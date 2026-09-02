52176ac41bbccee993f48dbe7afbd1a6c3a5ebf8
05484ym-max <05484ym@gmail.com>
2026-09-02 21:31:25 +0300

    Handle temporary debugging unlock command

--- dpc-app/app/src/main/java/org/mdmopen/dpc/CommandExecutor.kt
+++ dpc-app/app/src/main/java/org/mdmopen/dpc/CommandExecutor.kt
@@ -51,6 +51,10 @@ class CommandExecutor(private val context: Context) {
             PlayStoreGate.openForInstall(context, packageName, displayName)
             "נפתח Play Store עבור $displayName"
         }
+        "OPEN_DEBUGGING_TEMP" -> {
+            PolicyEnforcer(context).openDebuggingUntilNextSync()
+            "ניפוי באגים נפתח זמנית עד הסנכרון הבא"
+        }
         "RELEASE_DEVICE_OWNER" -> {
             PolicyEnforcer(context).releaseDeviceOwner()
             "ניהול המכשיר הוסר בהצלחה"
